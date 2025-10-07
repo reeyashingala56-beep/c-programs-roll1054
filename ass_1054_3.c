4bbafeb5062e60ea68d5fd315f7478062f120389
reeyashingala56-beep <reeyashingala56@gmail.com>
2025-10-07 21:51:53 +0530

    Update ass_1054_3.c

--- ass_1054_3.c
+++ ass_1054_3.c
@@ -5,10 +5,10 @@ int main()
 {
   char str1[50],str2[50];
   printf("Enter first string :");
-  gets(str1);
+  fgets(str1,,sizeof(str1),stdin);
   printf("Enter second string :");
-  gets(str2);
+  gets(str2,sizeof(str2),stdin);
   strcat(str1,str2);
-  printf("concatenate string :%s \n",str1);
+  printf("After concatenation:%s \n",str1);
   return 0;
 }
